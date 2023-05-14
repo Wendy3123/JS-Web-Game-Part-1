464ec7cb1ceb5c92e3fbf7a21868de2829a12f43
Wendy Wang <wendywangg928@gmail.com>
2023-05-14 11:38:11 -0400

    updated

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newimage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+function newimage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+function newItem(url, left, bottom){
+    let object = newimage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+newimage('assets/green-character.gif', 100, 100)
+newimage('assets/tree.png', 200, 300)
+newimage('assets/pillar.png', 350, 100)
+newimage('assets/pine-tree.png', 450, 200)
+newimage('assets/crate.png', 150, 200)
+newimage('assets/well.png', 500, 425)
+
+
+
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
